8cc0a79f2f87d32a7901950376a484600051993d
Mike Pitre <mikepitre1@gmail.com>
2023-10-03 00:50:01 -0400

    add Get library

--- Package.swift
+++ Package.swift
@@ -13,14 +13,15 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
-        .package(url: "https://github.com/CreateAPI/URLQueryEncoder", from: "0.2.0"),
+        .package(url: "https://github.com/CreateAPI/URLQueryEncoder", from: "0.2.1"),
+        .package(url: "https://github.com/kean/Get", from: "2.1.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Clerk",
-            dependencies: ["KeychainAccess", "URLQueryEncoder"],
+            dependencies: ["KeychainAccess", "URLQueryEncoder", "Get"],
             path: "Sources"
         ),
         .testTarget(
