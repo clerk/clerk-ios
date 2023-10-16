6a1c310a1b2a72558c306949aa7b7bac8c572dec
Mike Pitre <mikepitre1@gmail.com>
2023-10-16 15:41:11 -0400

    change ui of sign in example view

--- Example/ClerkExample/ExamplesListView.swift
+++ Example/ClerkExample/ExamplesListView.swift
@@ -13,28 +13,58 @@ import Clerk
 struct ExamplesListView: View {
     @EnvironmentObject private var clerk: Clerk
     
+    @State private var isDeletingClient = false
+    
     var body: some View {
         NavigationStack {
             List {
-                NavigationLink("Sign In / Sign Up", destination: SignInExampleView())
+                Section("Components") {
+                    Button {
+                        clerk.signInIsPresented = true
+                    } label: {
+                        Text("Sign In")
+                    }
+                    
+                    Button {
+                        clerk.signUpIsPresented = true
+                    } label: {
+                        Text("Sign Up")
+                    }
+                }
                 
-                Button(action: {
-                    Task { await deleteClientAction() }
-                }, label: {
-                    Text("Delete Client")
-                })
+                Section("Settings") {
+                    deleteClientButton
+                }
             }
             .navigationTitle("Clerk Examples")
         }
     }
     
+    @ViewBuilder
+    private var deleteClientButton: some View {
+        Button(action: {
+            Task { await deleteClientAction() }
+        }, label: {
+            ZStack(alignment: .leading) {
+                Text("Delete Client").opacity(isDeletingClient ? 0 : 1)
+                ProgressView().opacity(isDeletingClient ? 1 : 0)
+            }
+        })
+        .disabled(isDeletingClient)
+    }
+    
     private func deleteClientAction() async {
+        isDeletingClient = true
+        
         do {
             try await clerk
                 .client
                 .destroy()
+
+            isDeletingClient = false
         } catch {
             dump(error)
+            isDeletingClient = false
         }
     }
 }
