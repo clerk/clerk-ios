75607e5f82dbf40f6a5fa8e5da579848cb6b793d
Mike Pitre <mikepitre1@gmail.com>
2023-10-18 15:06:41 -0400

    fetch environment on app becoming active

--- Sources/UI/ViewModifiers/ClerkProviderModifier.swift
+++ Sources/UI/ViewModifiers/ClerkProviderModifier.swift
@@ -19,6 +19,7 @@ import Clerk
  */
 struct ClerkProviderModifier: ViewModifier {
     @Environment(\.clerkTheme) private var clerkTheme
+    @Environment(\.scenePhase) private var scenePhase
     
     @ObservedObject private var clerk = Clerk.shared
     
@@ -35,6 +36,11 @@ struct ClerkProviderModifier: ViewModifier {
             .task { try? await clerk.environment.get() }
             .signInView(isPresented: $clerk.signInIsPresented)
             .signUpView(isPresented: $clerk.signUpIsPresented)
+            .onChange(of: scenePhase) { phase in
+                if phase == .active {
+                    Task { try? await clerk.environment.get() }
+                }
+            }
             .environmentObject(clerk) // this must be the last modifier
     }
 }
