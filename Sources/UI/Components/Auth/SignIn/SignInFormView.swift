3d31446ff9b44ac37a4ac46a9879efb3f8f8b6ca
Mike Pitre <mikepitre1@gmail.com>
2023-11-03 10:18:36 -0400

    extract sign in form view

---
+++ Sources/UI/Components/Auth/SignIn/SignInFormView.swift
@@ -0,0 +1,104 @@
+//
+//  SignInFormView.swift
+//
+//
+//  Created by Mike Pitre on 11/3/23.
+//
+
+import SwiftUI
+import Clerk
+
+struct SignInFormView: View {
+    @EnvironmentObject private var clerk: Clerk
+    @EnvironmentObject private var clerkUIState: ClerkUIState
+    @Environment(\.clerkTheme) private var clerkTheme
+    
+    @State private var emailAddress: String = ""
+    @State private var phoneNumber: String = ""
+    @State private var displayingEmailEntry = true
+    
+    @FocusState private var focusedField: Field?
+    
+    private enum Field {
+        case email, phoneNumber
+    }
+    
+    private var signIn: SignIn {
+        clerk.client.signIn
+    }
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            VStack {
+                HStack {
+                    Text(displayingEmailEntry ? "Email address" : "Phone number")
+                        .contentTransition(.identity)
+                    Spacer()
+                    Button {
+                        displayingEmailEntry.toggle()
+                    } label: {
+                        Text(displayingEmailEntry ? "Use phone" : "Use email")
+                            .contentTransition(.identity)
+                    }
+                    .tint(clerkTheme.colors.primary)
+                }
+                .font(.footnote.weight(.medium))
+                
+                if displayingEmailEntry {
+                    CustomTextField(text: $emailAddress)
+                        .frame(height: 42)
+                        .textContentType(.emailAddress)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled(true)
+                        .focused($focusedField, equals: .email)
+                        .transition(.move(edge: .leading).combined(with: .opacity))
+                } else {
+                    PhoneNumberField(text: $phoneNumber)
+                        .focused($focusedField, equals: .phoneNumber)
+                        .frame(height: 42)
+                        .transition(.move(edge: .trailing).combined(with: .opacity))
+                }
+            }
+            .animation(.bouncy, value: displayingEmailEntry)
+            .onChange(of: displayingEmailEntry) { showingEmail in
+                if focusedField != nil {
+                    focusedField = showingEmail ? .email : .phoneNumber
+                }
+            }
+            
+            AsyncButton(options: [.disableButton, .showProgressView], action: {
+                if displayingEmailEntry {
+                    await signInAction(strategy: .emailCode(email: emailAddress))
+                } else {
+                    await signInAction(strategy: .phoneCode(phoneNumber: phoneNumber))
+                }
+            }) {
+                Text("CONTINUE")
+                    .font(.caption2.weight(.bold))
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 36)
+                    .foregroundStyle(.white)
+                    .background(clerkTheme.colors.primary)
+                    .clipShape(.rect(cornerRadius: 8, style: .continuous))
+            }
+        }
+    }
+    
+    private func signInAction(strategy: SignIn.CreateStrategy) async {
+        do {
+            KeyboardHelpers.dismissKeyboard()
+            try await signIn.create(strategy)
+            clerkUIState.presentedAuthStep = .signInFactorOne
+        } catch {
+            dump(error)
+        }
+    }
+}
+
+#Preview {
+    SignInFormView()
+        .padding()
+        .environmentObject(Clerk.mock)
+        .environmentObject(ClerkUIState())
+}
--- Sources/UI/Components/Auth/SignIn/SignInStartView.swift
+++ Sources/UI/Components/Auth/SignIn/SignInStartView.swift
@@ -16,20 +16,6 @@ struct SignInStartView: View {
     @Environment(\.clerkTheme) private var clerkTheme
     @Environment(\.dismiss) private var dismiss
     
-    @FocusState private var focusedField: Field?
-    
-    private enum Field {
-        case email, phoneNumber
-    }
-        
-    @State private var emailAddress: String = ""
-    @State private var phoneNumber: String = ""
-    @State private var displayingEmailEntry = true
-    
-    private var signIn: SignIn {
-        clerk.client.signIn
-    }
-    
     public var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 30) {
@@ -43,62 +29,8 @@ struct SignInStartView: View {
                 
                 OrDivider()
                 
-                VStack(spacing: 16) {
-                    VStack {
-                        HStack {
-                            Text(displayingEmailEntry ? "Email address" : "Phone number")
-                                .contentTransition(.identity)
-                            Spacer()
-                            Button {
-                                displayingEmailEntry.toggle()
-                            } label: {
-                                Text(displayingEmailEntry ? "Use phone" : "Use email")
-                                    .contentTransition(.identity)
-                            }
-                            .tint(clerkTheme.colors.primary)
-                        }
-                        .font(.footnote.weight(.medium))
-                        
-                        if displayingEmailEntry {
-                            CustomTextField(text: $emailAddress)
-                                .frame(height: 42)
-                                .textContentType(.emailAddress)
-                                .keyboardType(.emailAddress)
-                                .textInputAutocapitalization(.never)
-                                .autocorrectionDisabled(true)
-                                .focused($focusedField, equals: .email)
-                                .transition(.move(edge: .leading).combined(with: .opacity))
-                        } else {
-                            PhoneNumberField(text: $phoneNumber)
-                                .focused($focusedField, equals: .phoneNumber)
-                                .frame(height: 42)
-                                .transition(.move(edge: .trailing).combined(with: .opacity))
-                        }
-                    }
-                    .animation(.bouncy, value: displayingEmailEntry)
-                    .onChange(of: displayingEmailEntry) { showingEmail in
-                        if focusedField != nil {
-                            focusedField = showingEmail ? .email : .phoneNumber
-                        }
-                    }
-                    
-                    AsyncButton(options: [.disableButton, .showProgressView], action: {
-                        if displayingEmailEntry {
-                            await signInAction(strategy: .emailCode(email: emailAddress))
-                        } else {
-                            await signInAction(strategy: .phoneCode(phoneNumber: phoneNumber))
-                        }
-                    }) {
-                        Text("CONTINUE")
-                            .font(.caption2.weight(.bold))
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 36)
-                            .foregroundStyle(.white)
-                            .background(clerkTheme.colors.primary)
-                            .clipShape(.rect(cornerRadius: 8, style: .continuous))
-                    }
-                }
-                
+                SignInFormView()
+                                
                 HStack(spacing: 4) {
                     Text("No account?")
                         .font(.footnote)
@@ -136,20 +68,6 @@ struct SignInStartView: View {
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding(30)
             .background(.background)
-        }
-    }
-    
-    private var identifier: String {
-        displayingEmailEntry ? emailAddress : phoneNumber
-    }
-            
-    private func signInAction(strategy: SignIn.CreateStrategy) async {
-        do {
-            KeyboardHelpers.dismissKeyboard()
-            try await signIn.create(strategy)
-            clerkUIState.presentedAuthStep = .signInFactorOne
-        } catch {
-            dump(error)
         }
     }
 }
