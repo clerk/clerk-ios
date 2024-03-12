94793fc5212ba3c5d5ce7fa630836308e5320b20
Mike Pitre <mikepitre1@gmail.com>
2024-03-12 12:17:39 -0400

    update demo app instructions

--- ClerkDemo/ClerkDemo/HomeView.swift
+++ ClerkDemo/ClerkDemo/HomeView.swift
@@ -13,9 +13,12 @@ struct HomeView: View {
     var body: some View {
         NavigationStack {
             List {
-                Section {
-                    Text("1. Set your publishable key in the settings.")
-                    Text("2. Tap the user button to get started.")
+                Section("Instructions:") {
+                    Text("Create an account on [Clerk.com](https://www.clerk.com).")
+                    Text("Create an app.")
+                    Text("Copy your publishable key from the dashboard under the \"API Keys\" section.")
+                    Text("Paste your publishable key in the settings of this app and tap \"Save\".")
+                    Text("Tap the user button to get started.")
                 }
                 .font(.subheadline)
             }
