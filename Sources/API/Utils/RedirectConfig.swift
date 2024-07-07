e73cce44fb00758ba8e844c9145ec4055930b78f
Mike Pitre <mikepitre1@gmail.com>
2024-07-07 10:33:31 -0400

    move RedirectConfig out of Clerk extension

--- Sources/API/Utils/RedirectConfig.swift
+++ Sources/API/Utils/RedirectConfig.swift
@@ -7,24 +7,22 @@
 
 import Foundation
 
-extension Clerk {
+/// The configurable redirect settings. For example: `redirectUrl`, `callbackUrlScheme`
+public struct RedirectConfig {
     
-    public struct RedirectConfig {
-        /// - Parameter redirectUrl: The URL to redirect back to once an external flow has completed successfully or unsuccessfully. By default, this is set to `{YOUR_APPS_BUNDLE_IDENTIFIER}://callback`.
-        /// - Parameter callbackUrlScheme: The custom URL scheme that the app expects in the callback URL. By default, this is set to your app's bundle identifier.
-
-        public init(
-            redirectUrl: String = "\(Bundle.main.bundleIdentifier ?? "")://callback",
-            callbackUrlScheme: String = Bundle.main.bundleIdentifier ?? ""
-        ) {
-            self.redirectUrl = redirectUrl
-            self.callbackUrlScheme = callbackUrlScheme
-        }
-        
-        /// The URL to redirect back to once the external flow has completed successfully or unsuccessfully.
-        public var redirectUrl: String
-        /// The custom URL scheme that the app expects in the callback URL.
-        public var callbackUrlScheme: String
+    /// - Parameter redirectUrl: The URL to redirect back to once an external flow has completed successfully or unsuccessfully. By default, this is set to `{YOUR_APPS_BUNDLE_IDENTIFIER}://callback`.
+    /// - Parameter callbackUrlScheme: The custom URL scheme that the app expects in the callback URL. By default, this is set to your app's bundle identifier.
+    public init(
+        redirectUrl: String = "\(Bundle.main.bundleIdentifier ?? "")://callback",
+        callbackUrlScheme: String = Bundle.main.bundleIdentifier ?? ""
+    ) {
+        self.redirectUrl = redirectUrl
+        self.callbackUrlScheme = callbackUrlScheme
     }
     
+    /// The URL to redirect back to once the external flow has completed successfully or unsuccessfully.
+    public var redirectUrl: String
+    
+    /// The custom URL scheme that the app expects in the callback URL.
+    public var callbackUrlScheme: String
 }
