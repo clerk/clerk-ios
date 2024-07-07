1ce957610ef1c97bf6e6c3a69bb7e35cc39632ef
Mike Pitre <mikepitre1@gmail.com>
2024-07-07 10:34:04 -0400

    fix unused result for new environment return value

--- Sources/UI/Components/User/UserProfile/UserProfileAddExternalAccountView.swift
+++ Sources/UI/Components/User/UserProfile/UserProfileAddExternalAccountView.swift
@@ -75,7 +75,7 @@ struct UserProfileAddExternalAccountView: View {
         .clerkErrorPresenting($errorWrapper)
         .dismissButtonOverlay()
         .task {
-            try? await clerk.getEnvironment()
+            _ = try? await clerk.getEnvironment()
         }
     }
 }
