d11f377aaf5337c0ef7e09f7f9d3a232c48c0a56
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-01-10 17:47:35 -0500

    Mike/fix error throwing middleware (#156)
    * remove task
    
    * fix header middleware

--- Sources/API/APIClient/Middleware/Outgoing/HeaderMiddleware.swift
+++ Sources/API/APIClient/Middleware/Outgoing/HeaderMiddleware.swift
@@ -10,18 +10,17 @@ import SimpleKeychain
 
 struct HeaderMiddleware {
     
-    @MainActor
     static func process(_ request: inout URLRequest) async throws {
         // Set the device token on every request
         if let deviceToken = try? SimpleKeychain().string(forKey: "clerkDeviceToken") {
             request.setValue(deviceToken, forHTTPHeaderField: "Authorization")
         }
         
-        if Clerk.shared.debugMode, let client = Clerk.shared.client {
+        if await Clerk.shared.debugMode, let client = await Clerk.shared.client {
             request.setValue(client.id, forHTTPHeaderField: "x-clerk-client-id")
         }
         
-        request.setValue(deviceID, forHTTPHeaderField: "x-native-device-id")
+        await request.setValue(deviceID, forHTTPHeaderField: "x-native-device-id")
     }
     
 }
