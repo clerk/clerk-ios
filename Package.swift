e35f495ca1bddd5072d4d2405a9bbb2a964ee9ed
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-01-15 15:27:12 -0500

    update Get library

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/auth0/SimpleKeychain", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/kean/Get", .upToNextMajor(from: "2.1.6")),
+        .package(url: "https://github.com/kean/Get", .upToNextMajor(from: "2.2.1")),
         .package(url: "https://github.com/hmlongco/Factory", .upToNextMajor(from: "2.4.3")),
         .package(url: "https://github.com/onevcat/Kingfisher", .upToNextMajor(from: "8.1.3")),
         .package(url: "https://github.com/marmelroy/PhoneNumberKit", .upToNextMajor(from: "3.7.4"))
