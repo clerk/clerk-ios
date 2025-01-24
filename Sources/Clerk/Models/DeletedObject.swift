399f885e5fee4af8764c27942ab2ddb26ffcfb92
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-01-24 13:54:28 -0500

    docs for deleted object

--- Sources/Clerk/Models/DeletedObject.swift
+++ Sources/Clerk/Models/DeletedObject.swift
@@ -7,8 +7,14 @@
 
 import Foundation
 
+/// The DeletedObject class represents an item that has been deleted from the database.
 public struct DeletedObject: Decodable, Sendable {
-    let id: String?
+    /// The object type that has been deleted.
     let object: String?
+    
+    /// The ID of the deleted item.
+    let id: String?
+    
+    /// A boolean checking if the item has been deleted or not.
     let deleted: Bool?
 }
