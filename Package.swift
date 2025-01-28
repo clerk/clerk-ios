6adad3b4a93f58c007713bfcbfe9d481bfdfc123
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-01-28 15:32:42 -0500

    remove ClerkUI folder from package

--- Package.swift
+++ Package.swift
@@ -17,22 +17,16 @@ let package = Package(
         .library(name: "Clerk", targets: ["Clerk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/hmlongco/Factory", .upToNextMajor(from: "2.4.3")),
         .package(url: "https://github.com/kean/Get", .upToNextMajor(from: "2.2.1")),
-        .package(url: "https://github.com/onevcat/Kingfisher", .upToNextMajor(from: "8.1.3")),
-        .package(url: "https://github.com/marmelroy/PhoneNumberKit", .upToNextMajor(from: "3.7.4")),
         .package(url: "https://github.com/auth0/SimpleKeychain", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(
             name: "Clerk",
             dependencies: [
-                .product(name: "Algorithms", package: "swift-algorithms"),
                 .product(name: "Factory", package: "Factory"),
                 .product(name: "Get", package: "Get"),
-                .product(name: "Kingfisher", package: "Kingfisher"),
-                .product(name: "PhoneNumberKit", package: "PhoneNumberKit"),
                 .product(name: "SimpleKeychain", package: "SimpleKeychain")
             ],
             swiftSettings: [
