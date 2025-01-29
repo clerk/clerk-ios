6877fd8724d9fbe05d5272854bef8fc647fb6bb1
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-01-29 09:53:17 -0500

    remove public from string extensions

--- Sources/Clerk/Utils/String+Ext.swift
+++ Sources/Clerk/Utils/String+Ext.swift
@@ -9,13 +9,13 @@ import Foundation
 
 extension String {
     
-    public var capitalizedSentence: String {
+    var capitalizedSentence: String {
         let firstLetter = self.prefix(1).capitalized
         let remainingLetters = self.dropFirst().lowercased()
         return firstLetter + remainingLetters
     }
     
-    public func toJSON() -> JSON? {
+    func toJSON() -> JSON? {
         guard let data = self.data(using: .utf8, allowLossyConversion: false) else { return nil }
         let jsonObject = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers)
         return try? JSON(jsonObject as Any)
