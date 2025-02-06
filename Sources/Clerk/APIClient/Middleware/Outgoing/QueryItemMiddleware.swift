b2c22ceee11c182278e4e6dbe761db6683dd90f2
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-02-06 15:53:45 -0500

    Revert "remove global js version query item (#166)"
    This reverts commit 50b3618455e501b1a5fa33c3a519ddfc6df9be98.

--- Sources/Clerk/APIClient/Middleware/Outgoing/QueryItemMiddleware.swift
+++ Sources/Clerk/APIClient/Middleware/Outgoing/QueryItemMiddleware.swift
@@ -11,6 +11,7 @@ struct QueryItemMiddleware {
     
     static func process(_ request: inout URLRequest) {
         request.url?.append(queryItems: [.init(name: "_is_native", value: "true")])
+        request.url?.append(queryItems: [.init(name: "_clerk_js_version", value: "5.15.0")])
     }
     
 }
