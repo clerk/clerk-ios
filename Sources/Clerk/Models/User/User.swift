f2a9d56b0b18af8042c145388fa4266d77ce54c1
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-02-10 17:32:02 -0500

    org membership function formatting

--- Sources/Clerk/Models/User/User.swift
+++ Sources/Clerk/Models/User/User.swift
@@ -81,7 +81,7 @@ public struct User: Codable, Equatable, Sendable, Hashable {
         }
     }
     
-    /// An array of all the ```ExternalAccount``` objects associated with the user via OAuth. Note: This includes both verified & unverified external accounts.
+    /// An array of all the ``ExternalAccount`` objects associated with the user via OAuth. Note: This includes both verified & unverified external accounts.
     public let externalAccounts: [ExternalAccount]
     
     /// A getter for the user's list of verified external accounts.
@@ -98,10 +98,10 @@ public struct User: Codable, Equatable, Sendable, Hashable {
         }
     }
     
-    /// A list of ```EnterpriseAccount```s associated with the user.
+    /// A list of ``EnterpriseAccount``s associated with the user.
     public let enterpriseAccounts: [EnterpriseAccount]
     
-    /// A list of ```OrganizationMembership```s representing the list of organizations the user is a member of.
+    /// A list of ``OrganizationMembership``s representing the list of organizations the user is a member of.
     public let organizationMemberships: [OrganizationMembership]
     
     /// A boolean indicating whether the user has a password on their account.
