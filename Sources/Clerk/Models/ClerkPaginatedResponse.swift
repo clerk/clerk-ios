9b6d01480ecc1b4ae125d291b8578fd64bfc76b7
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-02-11 10:13:40 -0500

    paginated response docs

--- Sources/Clerk/Models/ClerkPaginatedResponse.swift
+++ Sources/Clerk/Models/ClerkPaginatedResponse.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
+/// An interface that describes the response of a method that returns a paginated list of resources.
 public struct ClerkPaginatedResponse<T: Decodable & Sendable>: Decodable, Sendable {
+    
+    /// An array that contains the fetched data.
     let data: [T]
+    
+    /// The total count of data that exists remotely.
     let totalCount: Int
 }
