387f4fa4a1c5e4d0a696fe8ed17ebce2e26e0abc
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-02-12 14:01:02 -0500

    actor to await in process assertion or attestation

--- Sources/Clerk/APIClient/Middleware/Incoming/DeviceAssertionMiddleware.swift
+++ Sources/Clerk/APIClient/Middleware/Incoming/DeviceAssertionMiddleware.swift
@@ -9,27 +9,47 @@ import Foundation
 import Get
 
 struct DeviceAssertionMiddleware {
+    private static let manager = Manager()
     
-    static func process(client: APIClient, shouldRetry task: URLSessionTask, error: any Error) async throws -> Bool {
-        
-        if let clerkAPIError = error as? ClerkAPIError {
-            switch clerkAPIError.code {
-                
-            case "requires_assertion":
-                try await AppAttestHelper.performAssertion()
-                return true
-                
-            case "requires_device_attestation":
-                try await AppAttestHelper.performDeviceAttestation()
-                try await AppAttestHelper.performAssertion()
-                return true
-                
-            default:
-                break
+    static func process(error: any Error) async throws -> Bool {
+        return try await manager.performDeviceAssertion(error: error)
+    }
+    
+    private actor Manager {
+        private var ongoingTask: Task<Bool, Error>?
+
+        func performDeviceAssertion(error: any Error) async throws -> Bool {
+            if let ongoingTask {
+                return try await ongoingTask.value
+            } else {
+                let newTask = Task<Bool, Error> {
+                    defer { ongoingTask = nil }
+                    
+                    if let clerkAPIError = error as? ClerkAPIError {
+                        switch clerkAPIError.code {
+                            
+                        case "requires_assertion":
+                            try await AppAttestHelper.performAssertion()
+                            return true
+
+                        case "requires_device_attestation":
+                            try await AppAttestHelper.performDeviceAttestation()
+                            try await AppAttestHelper.performAssertion()
+                            return true
+
+                        default:
+                            break
+                        }
+                    }
+                    
+                    return false
+                }
+
+                ongoingTask = newTask
+
+                return try await newTask.value
             }
         }
-        
-        return false
     }
     
 }
