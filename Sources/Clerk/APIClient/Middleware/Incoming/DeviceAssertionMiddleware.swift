bf41e7707290fdd68a7dde2ebd2a3f7a2e7a17a1
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-02-18 13:37:42 -0500

    device assertion error retry refactor

--- Sources/Clerk/APIClient/Middleware/Incoming/DeviceAssertionMiddleware.swift
+++ Sources/Clerk/APIClient/Middleware/Incoming/DeviceAssertionMiddleware.swift
@@ -17,51 +17,30 @@ struct DeviceAssertionMiddleware {
     
     private actor Manager {
         private var inFlightTask: Task<Bool, Error>?
-        private var inFlightErrorCode: String?
-
+        
         func performDeviceAssertion(task: URLSessionTask, error: any Error) async throws -> Bool {
-            guard
-                let clerkAPIError = error as? ClerkAPIError,
-                ["requires_assertion", "requires_device_attestation"].contains(clerkAPIError.code)
-            else {
+            guard let clerkAPIError = error as? ClerkAPIError, clerkAPIError.code == "requires_assertion" else {
                 return false
             }
             
-            // If there's already an ongoing task, decide if this error should wait or override
-            // "requires_device_attestation" should override "requires_assertion"
-            // therefore it breaks and creates a new task, not awaiting the inflight task
-            if let inFlightTask, let inFlightErrorCode {
-                switch (inFlightErrorCode, clerkAPIError.code) {
-                case ("requires_assertion", "requires_device_attestation"):
-                    break
-                default:
-                    return try await inFlightTask.value
-                }
+            if let inFlightTask {
+                return try await inFlightTask.value
             }
             
-            // Create a new task for the current error code
             let newTask = Task<Bool, Error> {
                 defer {
                     inFlightTask = nil
-                    inFlightErrorCode = nil
                 }
                 
                 switch clerkAPIError.code {
                 case "requires_assertion":
-                    try await AppAttestHelper.performAssertion()
-                    return true
-                    
-                case "requires_device_attestation":
-                    try await AppAttestHelper.performDeviceAttestation()
-                    try await AppAttestHelper.performAssertion()
-                    
-                    // if the original request was a client/verify, we dont need to retry it.
-                    // The above perform assertion uses the new attestation to verify.
-                    if let url = task.originalRequest?.url, url.path().hasSuffix("client/verify") {
-                        return false
+                    do {
+                        return try await handleRequiresAssertionError()
+                    } catch let error as ClerkAPIError where error.code == "requires_device_attestation" {
+                        return try await handleRequiresDeviceAttestationError(task: task)
+                    } catch {
+                        throw error
                     }
-                    
-                    return true
                     
                 default:
                     return false
@@ -69,9 +48,26 @@ struct DeviceAssertionMiddleware {
             }
             
             inFlightTask = newTask
-            inFlightErrorCode = clerkAPIError.code
             
             return try await newTask.value
+        }
+        
+        private func handleRequiresAssertionError() async throws -> Bool {
+            try await AppAttestHelper.performAssertion()
+            return true
+        }
+        
+        private func handleRequiresDeviceAttestationError(task: URLSessionTask) async throws -> Bool {
+            try await AppAttestHelper.performDeviceAttestation()
+            try await AppAttestHelper.performAssertion()
+            
+            // if the original request was a client/verify, we dont need to retry it.
+            // The above `performAssertion()` uses the new attestation to verify.
+            if let url = task.originalRequest?.url, url.path().hasSuffix("client/verify") {
+                return false
+            }
+            
+            return true
         }
     }
 }
