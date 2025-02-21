f6df238262cfc4a53bab30f57d07b0fd8489a76a
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-02-21 13:41:30 -0500

    Mike/swift dependencies (#171)
    * add swift-depencies package, move additional headers to headermiddleware
    
    * refactor ClerkAPI to use swift dependencies
    
    * put retry handlers in their own folder
    
    * refactor clerk api current method
    
    * remove factory library
    
    * put console message before throw
    
    * pr cleanup

--- Package.swift
+++ Package.swift
@@ -17,20 +17,22 @@ let package = Package(
         .library(name: "Clerk", targets: ["Clerk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/hmlongco/Factory", .upToNextMajor(from: "2.4.3")),
         .package(url: "https://github.com/kean/Get", .upToNextMajor(from: "2.2.1")),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
         .package(url: "https://github.com/auth0/SimpleKeychain", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(
             name: "Clerk",
             dependencies: [
-                .product(name: "Factory", package: "Factory"),
                 .product(name: "Get", package: "Get"),
+                .product(name: "Dependencies", package: "swift-dependencies"),
+                .product(name: "DependenciesMacros", package: "swift-dependencies"),
                 .product(name: "SimpleKeychain", package: "SimpleKeychain")
             ],
             swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency")
+              .enableUpcomingFeature("InferSendableFromCaptures"),
+              .enableExperimentalFeature("StrictConcurrency")
             ]
         ),
         .testTarget(
---
+++ Sources/Clerk/APIClient/ClerkAPIClient.swift
@@ -0,0 +1,137 @@
+//
+//  APIClient.swift
+//
+//
+//  Created by Mike Pitre on 10/2/23.
+//
+
+import Foundation
+import Get
+import SimpleKeychain
+import Dependencies
+import DependenciesMacros
+
+final class ClerkAPIClientDelegate: APIClientDelegate, Sendable {
+  
+  func client(_ client: APIClient, willSendRequest request: inout URLRequest) async throws {
+    await HeaderMiddleware.process(&request)
+    QueryItemMiddleware.process(&request)
+    try URLEncodedFormMiddleware.process(&request)
+  }
+  
+  func client(_ client: APIClient, validateResponse response: HTTPURLResponse, data: Data, task: URLSessionTask) throws {
+    DeviceTokenSavingMiddleware.process(response)
+    ClientSyncingMiddleware.process(data)
+    EventEmitterMiddleware.process(data)
+    try ErrorThrowingMiddleware.process(response, data: data)
+  }
+  
+  func client(_ client: APIClient, shouldRetry task: URLSessionTask, error: any Error, attempts: Int) async throws -> Bool {
+    guard attempts == 1 else {
+      return false
+    }
+    
+    if try await DeviceAssertionMiddleware.process(task: task, error: error) {
+      return true
+    }
+    
+    if try await InvalidAuthMiddleware.process(task: task, error: error) {
+      return true
+    }
+    
+    return false
+  }
+  
+}
+
+actor APIClientCache {
+  static var shared = APIClientCache()
+  
+  private var cache: [URL: APIClient] = [:]
+  private var _current: APIClient?
+  
+  var current: APIClient {
+    get throws {
+      guard let _current else {
+        dump("""
+        You need to set the current API Client before accessing it. 
+        You can do this by calling `client(for baseURL: String)`.
+        """
+        )
+        
+        throw ClerkClientError(
+          message: "Current API Client has not been initialized."
+        )
+      }
+      
+      return _current
+    }
+  }
+  
+  private func setCurrent(client: APIClient) {
+    _current = client
+  }
+  
+  func client(for baseURL: String) throws -> APIClient {
+    guard let url = URL(string: baseURL) else {
+      throw ClerkClientError(message: "Invalid base URL.")
+    }
+    
+    if let cachedClient = cache[url] {
+      return cachedClient
+    } else {
+      let newClient = APIClient(baseURL: url) { configuration in
+        configuration.delegate = ClerkAPIClientDelegate()
+        configuration.decoder = .clerkDecoder
+        configuration.encoder = .clerkEncoder
+      }
+      cache[url] = newClient
+      setCurrent(client: newClient)
+      return newClient
+    }
+  }
+  
+}
+
+@DependencyClient
+struct APIClientProvider {
+  var current: @Sendable () async throws -> APIClient
+  var client: @Sendable (_ baseUrl: String) async throws -> APIClient
+}
+
+extension APIClientProvider: DependencyKey, TestDependencyKey {
+  static var liveValue: APIClientProvider {
+    .init(
+      current: {
+        try await APIClientCache.shared.current
+      },
+      client: { baseUrl in
+        try await APIClientCache.shared.client(for: baseUrl)
+      }
+    )
+  }
+  
+  static var previewValue: APIClientProvider {
+    .init(
+      current: { .preview },
+      client: { _ in .preview }
+    )
+  }
+}
+
+extension DependencyValues {
+  var apiClientProvider: APIClientProvider {
+    get { self[APIClientProvider.self] }
+    set { self[APIClientProvider.self] = newValue }
+  }
+}
+
+extension APIClient {
+  static var preview: Self {
+    .init(baseURL: URL(string: "https://api.example.com")!) { configuration in
+      configuration.delegate = ClerkAPIClientDelegate()
+      configuration.encoder = .clerkEncoder
+      configuration.decoder = .clerkDecoder
+    }
+  }
+}
