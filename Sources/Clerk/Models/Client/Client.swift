e8987198d9eee53acac1b35f49242d04867c969d
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-02-24 11:12:14 -0500

    Mike/client tests (#175)
    * create ClientClient
    
    * add client tests

---
+++ Sources/Clerk/Models/Client/Client.swift
@@ -0,0 +1,51 @@
+//
+//  Client.swift
+//
+//
+//  Created by Mike Pitre on 10/2/23.
+//
+
+import Foundation
+import SimpleKeychain
+import Dependencies
+
+/// The Client object keeps track of the authenticated sessions in the current device. The device can be a browser, a native application or any other medium that is usually the requesting part in a request/response architecture.
+///
+/// The Client object also holds information about any sign in or sign up attempts that might be in progress, tracking the sign in or sign up progress.
+public struct Client: Codable, Sendable, Equatable {
+  
+  /// Unique identifier for this client.
+  public let id: String
+  
+  /// The current sign in attempt, or nil if there is none.
+  public let signIn: SignIn?
+  
+  /// The current sign up attempt, or nil if there is none.
+  public let signUp: SignUp?
+  
+  /// A list of sessions that have been created on this client.
+  public let sessions: [Session]
+  
+  /// A list of active sessions on this client.
+  public var activeSessions: [Session] {
+    sessions.filter { $0.status == .active }
+  }
+  
+  /// The ID of the last active Session on this client.
+  public let lastActiveSessionId: String?
+  
+  /// Timestamp of last update for the client.
+  public let updatedAt: Date
+    
+}
+
+extension Client {
+  
+  /// Retrieves the current client.
+  @discardableResult @MainActor
+  public static func get() async throws -> Client? {
+    @Dependency(\.clientClient) var clientClient
+    return try await clientClient.get()
+  }
+  
+}
---
+++ Sources/Clerk/Models/Client/ClientClient.swift
@@ -0,0 +1,38 @@
+//
+//  ClientClient.swift
+//  Clerk
+//
+//  Created by Mike Pitre on 2/23/25.
+//
+
+import Foundation
+import Dependencies
+import DependenciesMacros
+
+@DependencyClient
+struct ClientClient {
+  var get: @Sendable () async throws -> Client?
+}
+
+extension ClientClient: DependencyKey, TestDependencyKey {
+  
+  static var liveValue: ClientClient {
+    @Dependency(\.apiClientProvider) var apiClientProvider
+    
+    return .init(
+      get: {
+        let request = ClerkFAPI.v1.client.get
+        return try await apiClientProvider.current().send(request).value.response
+      }
+    )
+  }
+  
+  static let testValue = Self()
+}
+
+extension DependencyValues {
+  var clientClient: ClientClient {
+    get { self[ClientClient.self] }
+    set { self[ClientClient.self] = newValue }
+  }
+}
