94a94ca3ab9f5104285fd6ea3f5ea63ee703ca1c
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-02-26 12:30:56 -0500

    email address service and tests

---
+++ Sources/Clerk/Models/EmailAddress/EmailAddressService.swift
@@ -0,0 +1,52 @@
+//
+//  EmailAddressService.swift
+//  Clerk
+//
+//  Created by Mike Pitre on 2/26/25.
+//
+
+import Factory
+import Foundation
+
+struct EmailAddressService {
+  var prepareVerification: @MainActor (_ emailAddressId: String, _ strategy: EmailAddress.PrepareStrategy) async throws -> EmailAddress
+  var attemptVerification: @MainActor (_ emailAddressId: String, _ strategy: EmailAddress.AttemptStrategy) async throws -> EmailAddress
+  var destroy: @MainActor (_ emailAddressId: String) async throws -> DeletedObject
+}
+
+extension EmailAddressService {
+
+  static var liveValue: Self {
+    .init(
+      prepareVerification: { id, strategy in
+        let request = ClerkFAPI.v1.me.emailAddresses.id(id).prepareVerification.post(
+            queryItems: [.init(name: "_clerk_session_id", value: Clerk.shared.session?.id)],
+            body: strategy.requestBody
+        )
+        return try await Clerk.shared.apiClient.send(request).value.response
+      },
+      attemptVerification: { id, strategy in
+        let request = ClerkFAPI.v1.me.emailAddresses.id(id).attemptVerification.post(
+            queryItems: [.init(name: "_clerk_session_id", value: Clerk.shared.session?.id)],
+            body: strategy.requestBody
+        )
+        return try await Clerk.shared.apiClient.send(request).value.response
+      },
+      destroy: { id in
+        let request = ClerkFAPI.v1.me.emailAddresses.id(id).delete(
+            queryItems: [.init(name: "_clerk_session_id", value: Clerk.shared.session?.id)]
+        )
+        return try await Clerk.shared.apiClient.send(request).value.response
+      }
+    )
+  }
+  
+}
+
+extension Container {
+  
+  var emailAddressService: Factory<EmailAddressService> {
+    self { .liveValue }
+  }
+  
+}
---
+++ Tests/EmailAddressTests.swift
@@ -0,0 +1,71 @@
+import Testing
+import Foundation
+import Mocker
+
+@testable import Clerk
+@testable import Factory
+
+@Suite(.serialized) struct EmailAddressTests {
+  
+  init() {
+    Container.shared.reset()
+  }
+  
+  @Test func testEmailAddressPrepareVerificationRequest() async throws {
+    let requestHandled = LockIsolated(false)
+    let emailAddress = EmailAddress.mock
+    let originalUrl = mockBaseUrl.appending(path: "/v1/me/email_addresses/\(emailAddress.id)/prepare_verification")
+    var mock = Mock(url: originalUrl, ignoreQuery: true, contentType: .json, statusCode: 200, data: [
+      .post: try! JSONEncoder.clerkEncoder.encode(ClientResponse<EmailAddress>.init(response: .mock, client: .mock))
+    ])
+    mock.onRequestHandler = OnRequestHandler { request in
+      #expect(request.httpMethod == "POST")
+      #expect(request.url!.path() == "/v1/me/email_addresses/\(emailAddress.id)/prepare_verification")
+      #expect(request.url!.query()!.contains("_clerk_session_id"))
+      #expect(request.urlEncodedFormBody["strategy"] == "email_code")
+      requestHandled.setValue(true)
+    }
+    mock.register()
+    try await emailAddress.prepareVerification(strategy: .emailCode)
+    #expect(requestHandled.value)
+  }
+  
+  @Test func testEmailAddressAttemptVerificationRequest() async throws {
+    let requestHandled = LockIsolated(false)
+    let emailAddress = EmailAddress.mock
+    let code = "12345"
+    let originalUrl = mockBaseUrl.appending(path: "/v1/me/email_addresses/\(emailAddress.id)/attempt_verification")
+    var mock = Mock(url: originalUrl, ignoreQuery: true, contentType: .json, statusCode: 200, data: [
+      .post: try! JSONEncoder.clerkEncoder.encode(ClientResponse<EmailAddress>.init(response: .mock, client: .mock))
+    ])
+    mock.onRequestHandler = OnRequestHandler { request in
+      #expect(request.httpMethod == "POST")
+      #expect(request.url!.path() == "/v1/me/email_addresses/\(emailAddress.id)/attempt_verification")
+      #expect(request.url!.query()!.contains("_clerk_session_id"))
+      #expect(request.urlEncodedFormBody["code"] == code)
+      requestHandled.setValue(true)
+    }
+    mock.register()
+    try await emailAddress.attemptVerification(strategy: .emailCode(code: "12345"))
+    #expect(requestHandled.value)
+  }
+  
+  @Test func testEmailAddressDestroyRequest() async throws {
+    let requestHandled = LockIsolated(false)
+    let emailAddress = EmailAddress.mock
+    let originalUrl = mockBaseUrl.appending(path: "/v1/me/email_addresses/\(emailAddress.id)")
+    var mock = Mock(url: originalUrl, ignoreQuery: true, contentType: .json, statusCode: 200, data: [
+      .delete: try! JSONEncoder.clerkEncoder.encode(ClientResponse<DeletedObject>.init(response: .mock, client: .mock))
+    ])
+    mock.onRequestHandler = OnRequestHandler { request in
+      #expect(request.httpMethod == "DELETE")
+      #expect(request.url!.path() == "/v1/me/email_addresses/\(emailAddress.id)")
+      #expect(request.url!.query()!.contains("_clerk_session_id"))
+      requestHandled.setValue(true)
+    }
+    mock.register()
+    try await emailAddress.destroy()
+    #expect(requestHandled.value)
+  }
+  
+}
