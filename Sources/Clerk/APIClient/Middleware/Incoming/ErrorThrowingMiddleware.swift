f1b8a8c4960ec67996219a6c7081875f4c9c8031
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-04-02 18:27:12 -0400

    Mike/add trace id to errors (#216)
    * propagate clerk trace id to individual errors
    
    * bump version 0.54.1

--- Sources/Clerk/APIClient/Middleware/Incoming/ErrorThrowingMiddleware.swift
+++ Sources/Clerk/APIClient/Middleware/Incoming/ErrorThrowingMiddleware.swift
@@ -9,22 +9,25 @@ import Foundation
 import Get
 
 struct ErrorThrowingMiddleware {
+  
+  static func process(_ response: HTTPURLResponse, data: Data) throws {
     
-    static func process(_ response: HTTPURLResponse, data: Data) throws {
-        
-        // If our response is an error status code...
-        guard (200..<300).contains(response.statusCode) else {
-            
-            // ...and the response has a ClerkError body throw a custom clerk error
-            if let clerkError = try? JSONDecoder.clerkDecoder.decode(ClerkErrorResponse.self, from: data).errors.first {
-                throw clerkError
-            }
-
-            // ...else throw a generic api error
-            throw APIError.unacceptableStatusCode(response.statusCode)
-        }
-        
+    // If our response is an error status code...
+    guard (200..<300).contains(response.statusCode) else {
+      
+      // ...and the response has a ClerkError body throw a custom clerk error
+      if
+        let clerkErrorResponse = try? JSONDecoder.clerkDecoder.decode(ClerkErrorResponse.self, from: data),
+        var clerkAPIError = clerkErrorResponse.errors.first
+      {
+        clerkAPIError.clerkTraceId = clerkErrorResponse.clerkTraceId
+        throw clerkAPIError
+      }
+      
+      // ...else throw a generic api error
+      throw APIError.unacceptableStatusCode(response.statusCode)
     }
     
-    
+  }
+  
 }
