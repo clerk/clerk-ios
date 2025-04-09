ab37a7567eeb2167995a9e20f4473dc673822f88
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-04-09 17:00:34 -0400

    initial theme and localization setup

---
+++ Sources/Clerk/ClerkUI/Components/SignInView.swift
@@ -0,0 +1,82 @@
+//
+//  SignInView.swift
+//  Clerk
+//
+//  Created by Mike Pitre on 4/9/25.
+//
+
+import Factory
+import SwiftUI
+
+public struct SignInView: View {
+  @Environment(Clerk.self) private var clerk
+  @Environment(\.clerkTheme) private var theme
+
+  public init() {}
+
+  private var signInText: Text {
+    if let appName = clerk.environment.displayConfig?.applicationName {
+      return Text("Sign in to \(appName)", bundle: .module)
+    } else {
+      return Text("Sign in", bundle: .module)
+    }
+  }
+
+  public var body: some View {
+    ScrollView {
+      VStack(spacing: 0) {
+        Image(systemName: "star.square.fill")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 36, height: 36)
+          .padding(.bottom, 24)
+          .foregroundStyle(theme.colors.primary)
+        
+        signInText
+          .font(.title)
+          .fontWeight(.bold)
+          .multilineTextAlignment(.center)
+          .frame(minHeight: 32)
+          .padding(.bottom, 8)
+          .foregroundStyle(theme.colors.textPrimary)
+        
+        Text("Welcome back! Please sign in to continue", bundle: .module)
+          .font(.subheadline)
+          .multilineTextAlignment(.center)
+          .frame(minHeight: 18)
+          .foregroundStyle(theme.colors.textSecondary)
+      }
+      .padding(.horizontal, 24)
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .containerRelativeFrame([.vertical])
+    }
+  }
+}
+
+#Preview {
+  let _ = Container.shared.setupMocks()
+  SignInView()
+    .environment(Clerk.shared)
+    .environment(ClerkTheme.default)
+}
+
+#Preview("Custom Colors") {
+  let _ = Container.shared.setupMocks()
+  SignInView()
+    .environment(Clerk.shared)
+    .environment(\.clerkTheme,
+      ClerkTheme(
+        colors: .init(
+          primary: Color(.clerkPrimary)
+        )
+      )
+    )
+}
+
+#Preview("Spanish") {
+  let _ = Container.shared.setupMocks()
+  SignInView()
+    .environment(Clerk.shared)
+    .environment(ClerkTheme.default)
+    .environment(\.locale, .init(identifier: "es"))
+}
