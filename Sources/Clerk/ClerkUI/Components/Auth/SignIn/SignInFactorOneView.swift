e2fee49dfb9ac1862b45a2f3d2a0a23796bf1747
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-04-21 16:20:44 -0400

    get help view, factor sorting, starting sign in factor logic

--- Sources/Clerk/ClerkUI/Components/Auth/SignIn/SignInFactorOneView.swift
+++ Sources/Clerk/ClerkUI/Components/Auth/SignIn/SignInFactorOneView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct SignInFactorOneView: View {
+  @Environment(\.clerk) private var clerk
   @Environment(\.clerkTheme) private var theme
+  
+  let factor: Factor
 
+  var signIn: SignIn? {
+    clerk.client?.signIn
+  }
+  
+  @ViewBuilder
+  var viewForFactor: some View {
+    switch factor.strategy {
+    case "password":
+      SignInFactorOnePasswordView()
+    case "email_code":
+      SignInFactorOneCodeView(strategy: .emailCode)
+    case "phone_code":
+      SignInFactorOneCodeView(strategy: .phoneCode)
+    default:
+      SignInGetHelpView()
+    }
+  }
+  
   var body: some View {
-    SignInFactorOnePasswordView()
+    viewForFactor
   }
 }
 
 #Preview {
-  SignInFactorOneView()
+  SignInFactorOneView(factor: .init(strategy: "email_code"))
+    .environment(\.clerk, .mock)
 }
