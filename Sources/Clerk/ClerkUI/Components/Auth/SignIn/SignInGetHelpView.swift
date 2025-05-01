991eeeef8c7615782762876547e4542f73bbacd7
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-05-01 16:57:32 -0400

    mail to with support email

--- Sources/Clerk/ClerkUI/Components/Auth/SignIn/SignInGetHelpView.swift
+++ Sources/Clerk/ClerkUI/Components/Auth/SignIn/SignInGetHelpView.swift
@@ -7,46 +7,60 @@
 
 #if canImport(SwiftUI)
 
-import SwiftUI
+  import SwiftUI
 
-struct SignInGetHelpView: View {
-  @Environment(\.authState) private var authState
-  @Environment(\.clerkTheme) private var theme
+  struct SignInGetHelpView: View {
+    @Environment(\.clerkTheme) private var theme
+    @Environment(\.authState) private var authState
+    @Environment(\.supportEmail) private var supportEmail
 
-  var body: some View {
-    ScrollView {
-      VStack(spacing: 0) {
-        AppLogoView()
-          .frame(maxHeight: 44)
-          .padding(.bottom, 24)
+    var body: some View {
+      ScrollView {
+        VStack(spacing: 0) {
+          AppLogoView()
+            .frame(maxHeight: 44)
+            .padding(.bottom, 24)
 
-        VStack(spacing: 8) {
-          HeaderView(style: .title, text: "Get help")
-          HeaderView(style: .subtitle, text: "If you have trouble signing into your account, email us and we will work with you to restore access as soon as possible.")
-        }
-        .padding(.bottom, 32)
-
-        VStack(spacing: 16) {
-          Button {
-            //
-          } label: {
-            Text("Email support", bundle: .module)
-              .frame(maxWidth: .infinity)
+          VStack(spacing: 8) {
+            HeaderView(style: .title, text: "Get help")
+            HeaderView(style: .subtitle, text: "If you have trouble signing into your account, email us and we will work with you to restore access as soon as possible.")
           }
-          .buttonStyle(.primary())
+          .padding(.bottom, 32)
+
+          VStack(spacing: 16) {
+            Button {
+              openEmail(to: supportEmail)
+            } label: {
+              Text("Email support", bundle: .module)
+                .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.primary())
+          }
+          .padding(.bottom, 32)
+
+          SecuredByClerkView()
         }
-        .padding(.bottom, 32)
-
-        SecuredByClerkView()
+        .padding(16)
       }
-      .padding(16)
+      .background(theme.colors.background)
     }
-    .background(theme.colors.background)
   }
-}
 
-#Preview {
-  SignInGetHelpView()
-}
+  extension SignInGetHelpView {
+
+    func openEmail(to: String) {
+      let urlString = "mailto:\(to)"
+
+      if let url = URL(string: urlString) {
+        UIApplication.shared.open(url)
+      }
+    }
+
+  }
+
+  #Preview {
+    SignInGetHelpView()
+      .environment(\.clerk, .mock)
+  }
 
 #endif
