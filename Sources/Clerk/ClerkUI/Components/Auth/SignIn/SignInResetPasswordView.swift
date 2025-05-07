26eb407a9121d5f85391955f2e09f242dd6ae3f9
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-05-07 19:21:01 -0400

    forgot password, set new password UI

---
+++ Sources/Clerk/ClerkUI/Components/Auth/SignIn/SignInResetPasswordView.swift
@@ -0,0 +1,104 @@
+//
+//  SignInResetPasswordView.swift
+//  Clerk
+//
+//  Created by Mike Pitre on 5/7/25.
+//
+
+#if os(iOS)
+
+  import SwiftUI
+
+  struct SignInResetPasswordView: View {
+    @Environment(\.clerk) private var clerk
+    @Environment(\.clerkTheme) private var theme
+    @Environment(\.authState) private var authState
+
+    @FocusState private var focusedField: Field?
+    @State private var fieldError: Error?
+
+    enum Field {
+      case new, confirm
+    }
+
+    var body: some View {
+      @Bindable var authState = authState
+
+      ScrollView {
+        VStack(spacing: 0) {
+          AppLogoView()
+            .frame(maxHeight: 44)
+            .padding(.bottom, 24)
+
+          HeaderView(style: .title, text: "Set new password")
+            .padding(.bottom, 32)
+
+          VStack(spacing: 24) {
+            ClerkTextField(
+              "New password",
+              text: $authState.newPassword,
+              isSecure: true,
+              fieldState: fieldError != nil ? .error : .default
+            )
+            .textContentType(.newPassword)
+            .textInputAutocapitalization(.never)
+            .focused($focusedField, equals: .new)
+            .onFirstAppear {
+              focusedField = .new
+            }
+
+            VStack(spacing: 8) {
+              ClerkTextField(
+                "Confirm password",
+                text: $authState.confirmNewPassword,
+                isSecure: true,
+                fieldState: fieldError != nil ? .error : .default
+              )
+              .textContentType(.newPassword)
+              .textInputAutocapitalization(.never)
+              .focused($focusedField, equals: .confirm)
+
+              if let fieldError {
+                ErrorText(error: fieldError, alignment: .leading)
+                  .font(theme.fonts.subheadline)
+                  .transition(.blurReplace.animation(.default.speed(2)))
+                  .id(fieldError.localizedDescription)
+              }
+            }
+
+            AsyncButton {
+              // reset password
+            } label: { isRunning in
+              HStack(spacing: 4) {
+                Text("Reset password", bundle: .module)
+                Image("icon-triangle-right", bundle: .module)
+                  .foregroundStyle(theme.colors.textOnPrimaryBackground)
+                  .opacity(0.6)
+              }
+              .frame(maxWidth: .infinity)
+              .overlayProgressView(isActive: isRunning) {
+                SpinnerView(color: theme.colors.textOnPrimaryBackground)
+              }
+            }
+            .buttonStyle(.primary())
+            .disabled(authState.password.isEmpty)
+            .simultaneousGesture(TapGesture())
+          }
+          .padding(.bottom, 32)
+
+          SecuredByClerkView()
+        }
+        .padding(16)
+      }
+      .background(theme.colors.background)
+      .sensoryFeedback(.error, trigger: fieldError?.localizedDescription)
+      .navigationBarBackButtonHidden()
+    }
+  }
+
+  #Preview {
+    SignInResetPasswordView()
+      .environment(\.clerkTheme, .clerk)
+  }
+
+#endif
