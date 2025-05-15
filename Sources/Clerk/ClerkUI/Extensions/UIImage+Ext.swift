19899a32cf9f112bcc759a8d6627ee070f100170
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-05-15 19:23:27 -0400

    move uiimage extension to own file

---
+++ Sources/Clerk/ClerkUI/Extensions/UIImage+Ext.swift
@@ -0,0 +1,32 @@
+//
+//  UIImage+Ext.swift
+//  Clerk
+//
+//  Created by Mike Pitre on 5/15/25.
+//
+
+#if os(iOS)
+
+import Foundation
+import SwiftUI
+
+extension UIImage {
+  func resizedMaintainingAspectRatio(to targetSize: CGSize) -> UIImage {
+    let aspectRatio = size.width / size.height
+    let targetAspectRatio = targetSize.width / targetSize.height
+
+    let newSize: CGSize
+    if aspectRatio > targetAspectRatio {
+      newSize = CGSize(width: targetSize.width, height: targetSize.width / aspectRatio)
+    } else {
+      newSize = CGSize(width: targetSize.height * aspectRatio, height: targetSize.height)
+    }
+
+    let renderer = UIGraphicsImageRenderer(size: newSize)
+    return renderer.image { _ in
+      self.draw(in: CGRect(origin: .zero, size: newSize))
+    }
+  }
+}
+
+#endif
