68e9bea96cab52ae9560e300e8eb758feb53ca49
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-05-16 17:37:52 -0400

    add email to account

---
+++ Sources/Clerk/ClerkUI/Extensions/String+Ext.swift
@@ -0,0 +1,34 @@
+//
+//  String+Ext.swift.swift
+//  Clerk
+//
+//  Created by Mike Pitre on 4/29/25.
+//
+
+#if os(iOS)
+
+  import Factory
+  import Foundation
+  import PhoneNumberKit
+
+  extension String {
+    var formattedAsPhoneNumberIfPossible: String {
+      let utility = Container.shared.phoneNumberUtility()
+      let partialFormatter = PartialFormatter(utility: utility, withPrefix: true)
+      return partialFormatter.formatPartial(self).nonBreaking
+    }
+
+    var nonBreaking: String {
+      self
+        .replacingOccurrences(of: " ", with: "\u{00A0}")
+        .replacingOccurrences(of: "-", with: "\u{2011}")
+    }
+
+    var capitalizedSentence: String {
+      let firstLetter = self.prefix(1).capitalized
+      let remainingLetters = self.dropFirst().lowercased()
+      return firstLetter + remainingLetters
+    }
+  }
+
+#endif
