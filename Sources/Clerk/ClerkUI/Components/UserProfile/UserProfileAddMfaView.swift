d4e85182df77aa60b0f784ca3248f457da0c1186
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-06-03 15:29:56 -0400

    choose mfa sheet

---
+++ Sources/Clerk/ClerkUI/Components/UserProfile/UserProfileAddMfaView.swift
@@ -0,0 +1,109 @@
+//
+//  UserProfileAddMfaView.swift
+//  Clerk
+//
+//  Created by Mike Pitre on 6/3/25.
+//
+
+#if os(iOS)
+
+  import SwiftUI
+
+  struct UserProfileAddMfaView: View {
+    @Environment(\.clerk) private var clerk
+    @Environment(\.clerkTheme) private var theme
+    @Environment(\.dismiss) private var dismiss
+
+    @Binding private var contentHeight: CGFloat
+    @State private var error: Error?
+
+    init(contentHeight: Binding<CGFloat> = .constant(0)) {
+      self._contentHeight = contentHeight
+    }
+
+    var environment: Clerk.Environment {
+      clerk.environment
+    }
+
+    var body: some View {
+      NavigationStack {
+        ScrollView {
+          VStack(spacing: 24) {
+            Text("Choose how you’d like to receive your two-step verification code.", bundle: .module)
+              .font(theme.fonts.subheadline)
+              .foregroundStyle(theme.colors.textSecondary)
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .fixedSize(horizontal: false, vertical: true)
+              .padding(.horizontal, 24)
+
+            VStack(spacing: 0) {
+              Group {
+                if environment.mfaPhoneCodeIsEnabled {
+                  Button {
+                    //
+                  } label: {
+                    UserProfileRowView(icon: "icon-phone", text: "SMS code")
+                  }
+                }
+
+                if environment.mfaAuthenticatorAppIsEnabled {
+                  Button {
+                    //
+                  } label: {
+                    UserProfileRowView(icon: "icon-key", text: "Authenticator application")
+                  }
+                }
+              }
+              .overlay(alignment: .bottom) {
+                Rectangle()
+                  .frame(height: 1)
+                  .foregroundStyle(theme.colors.border)
+              }
+              .buttonStyle(.pressedBackground)
+              .simultaneousGesture(TapGesture())
+            }
+            .overlay(alignment: .top) {
+              Rectangle()
+                .frame(height: 1)
+                .foregroundStyle(theme.colors.border)
+            }
+          }
+          .padding(.vertical, 24)
+          .background(theme.colors.background)
+          .clerkErrorPresenting($error)
+          .navigationBarTitleDisplayMode(.inline)
+          .toolbarBackground(.visible, for: .navigationBar)
+          .toolbarBackground(theme.colors.background, for: .navigationBar)
+          .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+              Button("Cancel") {
+                dismiss()
+              }
+              .foregroundStyle(theme.colors.primary)
+            }
+
+            ToolbarItem(placement: .principal) {
+              Text("Add two-step verification", bundle: .module)
+                .font(theme.fonts.headline)
+                .foregroundStyle(theme.colors.text)
+            }
+          }
+          .onGeometryChange(for: CGFloat.self) { proxy in
+            proxy.size.height
+          } action: { newValue in
+            contentHeight = newValue + UITabBarController().tabBar.frame.size.height + 20
+          }
+        }
+        .scrollBounceBehavior(.basedOnSize)
+      }
+      .presentationBackground(theme.colors.background)
+    }
+  }
+
+  #Preview {
+    UserProfileAddMfaView()
+      .environment(\.clerk, .mock)
+      .environment(\.clerkTheme, .clerk)
+  }
+
+#endif
