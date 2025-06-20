3db7e5bb4d7c824fad8c04e78a629dfcfd2af2ef
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-06-20 15:13:25 -0400

    string helpers for identifier types

--- Sources/Clerk/ClerkUI/Extensions/String+Ext.swift
+++ Sources/Clerk/ClerkUI/Extensions/String+Ext.swift
@@ -29,9 +29,20 @@
       let remainingLetters = self.dropFirst().lowercased()
       return firstLetter + remainingLetters
     }
-    
+
     var isEmptyTrimmed: Bool {
       trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
+
+    var isEmailAddress: Bool {
+      let emailRegex = #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"#
+      let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+      return emailPredicate.evaluate(with: self)
+    }
+    
+    var isPhoneNumber: Bool {
+      let utility = Container.shared.phoneNumberUtility()
+      return utility.isValidPhoneNumber(self)
     }
   }
 
