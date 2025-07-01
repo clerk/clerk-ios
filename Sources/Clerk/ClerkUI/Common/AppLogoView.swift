2af404e631763fa3bde312360129c98c8fe33dc0
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-07-01 01:03:47 -0400

    dont show placeholder in debug run

--- Sources/Clerk/ClerkUI/Common/AppLogoView.swift
+++ Sources/Clerk/ClerkUI/Common/AppLogoView.swift
@@ -16,13 +16,13 @@ struct AppLogoView: View {
   var body: some View {
     KFImage(URL(string: clerk.environment.displayConfig?.logoImageUrl ?? ""))
       .resizable()
-      .placeholder {
-        #if DEBUG
-        Image(systemName: "circle.square.fill")
-          .resizable()
-          .scaledToFit()
-        #endif
-      }
+//      .placeholder {
+//        #if DEBUG
+//        Image(systemName: "circle.square.fill")
+//          .resizable()
+//          .scaledToFit()
+//        #endif
+//      }
       .scaledToFit()
   }
 }
