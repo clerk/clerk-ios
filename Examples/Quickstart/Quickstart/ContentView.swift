8f6f0e7e3ab1caf9586b0c5c6e17031673004ffe
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-07-30 10:36:40 -0400

    quickstart implementation

--- Examples/Quickstart/Quickstart/ContentView.swift
+++ Examples/Quickstart/Quickstart/ContentView.swift
@@ -5,17 +5,27 @@
 //  Created by Mike Pitre on 7/30/25.
 //
 
+import Clerk
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.clerk) private var clerk
+    @State private var authViewIsPresented = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if clerk.user != nil {
+                UserButton()
+                    .frame(width: 36, height: 36)
+            } else {
+                Button("Sign in") {
+                    authViewIsPresented = true
+                }
+            }
         }
-        .padding()
+        .sheet(isPresented: $authViewIsPresented) {
+            AuthView()
+        }
     }
 }
 
