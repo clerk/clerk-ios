8e437c3d16e4741a1ef369c99a8f382fa126f3d2
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2025-12-19 17:07:38 -0500

    add resource not found to invalid auth codes

--- Sources/ClerkKit/Networking/Middleware/ClerkInvalidAuthResponseMiddleware.swift
+++ Sources/ClerkKit/Networking/Middleware/ClerkInvalidAuthResponseMiddleware.swift
@@ -9,11 +9,13 @@ import Foundation
 
 /// When the API indicates authentication is invalid, re-sync the client state unless we already attempted it.
 struct ClerkInvalidAuthResponseMiddleware: NetworkResponseMiddleware {
+  let invalidAuthCodes = ["authentication_invalid", "resource_not_found"]
+
   func validate(_: HTTPURLResponse, data: Data, for request: URLRequest) throws {
     guard
       let clerkErrorResponse = try? JSONDecoder.clerkDecoder.decode(ClerkErrorResponse.self, from: data),
       let clerkAPIError = clerkErrorResponse.errors.first,
-      clerkAPIError.code == "authentication_invalid"
+      invalidAuthCodes.contains(clerkAPIError.code)
     else {
       return
     }
