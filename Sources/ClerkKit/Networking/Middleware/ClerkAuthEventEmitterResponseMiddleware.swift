88bc4d409468c38ae11e82503115372401e38c09
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2026-03-06 17:29:29 -0500

    Clarify auth middleware decoding

--- Sources/ClerkKit/Networking/Middleware/ClerkAuthEventEmitterResponseMiddleware.swift
+++ Sources/ClerkKit/Networking/Middleware/ClerkAuthEventEmitterResponseMiddleware.swift
@@ -7,16 +7,18 @@ import Foundation
 
 struct ClerkAuthEventEmitterResponseMiddleware: ClerkResponseMiddleware {
   func validate(_: HTTPURLResponse, data: Data, for _: URLRequest) async throws {
-    if let signIn = try? JSONDecoder.clerkDecoder.decode(ClientResponse<SignIn>.self, from: data).response,
-       signIn.status == .complete
-    {
-      await Clerk.shared.auth.send(.signInCompleted(signIn: signIn))
-    }
-
     if let signUp = try? JSONDecoder.clerkDecoder.decode(ClientResponse<SignUp>.self, from: data).response,
        signUp.status == .complete
     {
       await Clerk.shared.auth.send(.signUpCompleted(signUp: signUp))
+      return
+    }
+
+    if let signIn = try? JSONDecoder.clerkDecoder.decode(ClientResponse<SignIn>.self, from: data).response,
+       signIn.status == .complete
+    {
+      await Clerk.shared.auth.send(.signInCompleted(signIn: signIn))
+      return
     }
 
     if let session = try? JSONDecoder.clerkDecoder.decode(ClientResponse<Session>.self, from: data).response,
