ad9545202ee24477a20bceccdb20461913788216
Sean Perez <sean@clerk.dev>
2026-05-01 12:03:45 -0400

    Bump SDK version to 1.1.1 (#408)

--- Sources/ClerkKit/Utils/Version.swift
+++ Sources/ClerkKit/Utils/Version.swift
@@ -5,6 +5,6 @@
 import Foundation
 
 extension Clerk {
-  public nonisolated static let sdkVersion: String = "1.1.0"
+  public nonisolated static let sdkVersion: String = "1.1.1"
   nonisolated static let apiVersion: String = "2025-11-10"
 }
