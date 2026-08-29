1b58165de0b6519ed3fb226f41763b354bca95d5
clerk-cookie <cookie@clerk.dev>
2026-08-29 23:08:47 +0000

    chore(release): 1.5.1

--- Sources/ClerkKit/Utils/Version.swift
+++ Sources/ClerkKit/Utils/Version.swift
@@ -5,6 +5,6 @@
 import Foundation
 
 extension Clerk {
-  public nonisolated static let sdkVersion: String = "1.5.0"
+  public nonisolated static let sdkVersion: String = "1.5.1"
   nonisolated static let apiVersion: String = "2026-08-20"
 }
